d4b8450ea45795325445b83e8d8b67deeb2634e5
Stacy Smith <stacy@kit10.us>
2026-09-04 08:53:39 -0700

    Pushing volatile changes as workstation is unstable.

--- medx11/Texture.cpp
+++ medx11/Texture.cpp
@@ -9,6 +9,7 @@
 #include <me/exception/NotImplemented.h>
 #include <me/exception/FailedToLock.h>
 #include <qxml/Document.h>
+#include <unify/Cast.h>
 
 // MS agressive macros.
 #ifdef LoadImage
@@ -212,7 +213,7 @@ void Texture::CreateFromSize()
 	if (WIN_FAILED( result ))
 	{
 		Destroy();
-		throw unify::Exception( "Failed to create texture of size " + unify::Cast< std::string >( width ) + "x" + unify::Cast< std::string >( height ) + "!" );
+		throw unify::Exception( "Failed to create texture of size " + *unify::ToString( width ) + "x" + *unify::ToString( height ) + "!" );
 	}
 
 	D3D11_SAMPLER_DESC colorMapDesc{};
